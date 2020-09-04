f17511cec80e46ceea8dcaecb85d4f3b77614a90
israbsal <desintegrado19@gmail.com>
2020-09-04 09:58:50 -0500

    Funcionando

--- Practica 1/ejercicio2.sh
+++ Practica 1/ejercicio2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-procesos=$(ps -Ao comm --sort=-pcpu | head -n 6)
+#echo "$1"
+procesos=$(ps -U $1 -u $1 -o user,comm,%cpu --sort=-pcpu | head -n 6) #user
 echo "$procesos"
--- Practica 1/ejercicio3.sh
+++ Practica 1/ejercicio3.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#echo "$1"
-procesos=$(ps -U $1 -u $1 -o user,comm,%cpu --sort=-pcpu | head -n 11) #user
+procesos=$(ps -Ao user,comm,%cpu --sort=-pcpu | head -n 11)
 echo "$procesos"
