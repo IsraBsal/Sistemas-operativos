116c7e3aa46fc5903e97ff63c9b274d961c1718d
IsraBsal <53355895+IsraBsal@users.noreply.github.com>
2020-09-05 22:46:34 -0500

    Update ejercicio5.sh
    Funcionando

--- Practica 1/ejercicio5.sh
+++ Practica 1/ejercicio5.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-n=$1 #Es el numero que nos dan en la llamada y $1 jala el numero
-#Inicia la logica
+n=$1 #Hijos que le pasamos
 
-#Obtenemos el total de los padres para delimitar
-num_total_padres=$(ps -ef --sort ppid --no-headers | wc -l)
-for (( i=0;i<=num_total_padres;i++ ))
+#Número total de procesos que hay
+numTotalPadres=$(ps -ef --no-headers | wc -l)
+
+#Contador de procesos
+proceso_con_n_hijos=0
+
+for(( i=1;i<numTotalPadres;i++ ))
 do
-  num_hijos=$(ps --ppid $i | wc -l)-1
-
-
-
+    j=$(($i+1)) #j es utilizado para moverme entre el numero de pid del cual quiero saber cuantos hijos tiene
+    #Proceso contiene el pid
+    proceso=$(ps -eo ppid --sort ppid --no-headers | sed -n ''$j'p') #El comando nos tira una linea donde esta el pid, el $jp nos da pid por pid
+    if [ $proceso -gt 0 ] #Saltamos el proceso 0 y el encabezado
+    then
+      #Numero de procesos que tiene un proceso padre
+      numHijos=$(ps --ppid $proceso | wc -l)
+      if [ $numHijos == $n ]
+      then
+          proceso_con_n_hijos=$(($proceso_con_n_hijos + 1))
+      fi
+    fi
 done
+echo "Procesos con $n hijos: $proceso_con_n_hijos"
