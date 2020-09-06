c1bfb21da8b2694789c0578c66c1265c35a45a36
israbsal <desintegrado19@gmail.com>
2020-09-05 20:02:50 -0500

    Ejercicio 5 iniciando.

--- Practica 1/ejercicio5.sh
+++ Practica 1/ejercicio5.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+n=$1 #Es el numero que nos dan en la llamada y $1 jala el numero
+#Inicia la logica
+
+#Obtenemos el total de los padres para delimitar
+num_total_padres=$(ps -ef --sort ppid --no-headers | wc -l)
+for (( i=0;i<=num_total_padres;i++ ))
+do
+  num_hijos=$(ps --ppid $i | wc -l)-1
+
+
+
+done
