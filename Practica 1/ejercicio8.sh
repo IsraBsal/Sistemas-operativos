3b8724e47762f660d5bdbce50d0f6e9ce50e0a7c
IsraBsal <53355895+IsraBsal@users.noreply.github.com>
2020-09-10 16:19:16 -0500

    Ejercicio 8 empezando
    Comienza la parte de rellenar el arreglo en bash.

---
+++ Practica 1/ejercicio8.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+n=$1
+./binario $n &
+pid_padre=$(pgrep binario | head -n 1 ) #Obtenemos el pid del primer proceso creado
+echo "$pid_padre"
+#Comando para comprobar la poda en el arbol
+ps -eo pid,comm | grep binario | head -n 1 | awk {'print $1'} | xargs pstree -c
+
+hijo_izquiero=$(ps --ppid $pid_padre | sed -n '2p' | awk {'print $1'} )
+hijo_derecho=$(ps --ppid $pid_padre | sed -n '3p'| awk {'print $1'} )
+
+echo "Hijo izquiero $hijo_izquiero"
+echo "Hijo derecho $hijo_derecho"
+
+killall binario
+
+#pstree -aps | grep binario
