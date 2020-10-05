18a6425ba4cc2ef52dd0664dcb08e18b1791f7ae
IsraBsal <53355895+IsraBsal@users.noreply.github.com>
2020-10-05 18:30:05 -0500

    Funcionando
    Version final para entrega

--- saltocaballo2.c
+++ saltocaballo2.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
 
+int soluciones=0;
 int desplazamiento[8][2]={{-2,-1},{-2,1},{-1,-2},{-1,2},{1,-2},{1,2},{2,1},{2,-1}};
 
 void mostrar(int **tablero,int n){
@@ -55,60 +60,81 @@ int backtrackCaballos(int **tablero, int x, int y, int salto, int n){
 }
 
 
-
-
-
-
 int main(int argc, char **argv){
-  int n;
-  int i, j, k;
-  int **tablero;
-  int soluciones=0, total=0;
-  pid_t pid;
-  FILE *fp;
+    int *total = (int*)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE,
+               MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-  if(argc!=2){
-  
-     printf("Error, ejectutar como ./ejecutable <Tam tablero>, por ejemplo:\n./saltos 7\n");
-  }
-     
-  n= atoi(argv[1]);
-
-  tablero = (int **)malloc(sizeof(int *)*n);
-  for(i=0;i<n;i++)
-     tablero[i]=(int *)malloc(sizeof(int)*n);
-  
-  pid=fork();
-  for(j=0;j<n;j++){
-    for(k=0;k<n;k++){
-      if(pid==0){
-        //printf("pid %d\n",getppid());
-        inicializa(tablero,n);
-        tablero[j][k] = 1;
-        soluciones=backtrackCaballos(tablero,j,k,2,n);
-        total=total+soluciones;
-        //printf("Soluciones en %d %d con pid %d = %d\n",j,k,getpid(),soluciones);
-        //printf("Parcial de soluciones de todo el tablero = %d con pid %d \n",total,getpid());
-        char cadena[40];
-        char cadena2[58];
-        sprintf(cadena,"\nSoluciones en %d %d con pid %d = %d\n",j,k,getpid(),soluciones);
-        sprintf(cadena2,"\nParcial de soluciones de todo el tablero = %d con pid %d \n",total,getpid());
-        fp = fopen ( "fichero.txt", "r+" );
-        fwrite(cadena,sizeof(char),sizeof(cadena),fp);
-        fwrite(cadena2,sizeof(char),sizeof(cadena2),fp);
-        pid=fork();
-        //sleep(1);
-      }
-      /*else{
-            char cadena1[88];
-            sprintf(cadena1,"\nParcial de soluciones de todo el tablero = %d con pid %d SOY EL PAPA Y ESTO ES EL FINAL\n",total,getpid());
-            fp = fopen ( "fichero.txt", "r+" );
-            fwrite(cadena1,sizeof(char),sizeof(cadena1),fp);
-            
-        }*/
+	int tid;
+	int n;
+    int i, j, k; //Contadores para irnos moviendo
+    int **tablero;
+   
+    pid_t pid;
+    FILE *fp; 
+    
+    if(argc!=2){
+        printf("Error, ejectutar como ./ejecutable <Tam tablero>, por ejemplo:\n./saltos 7\n");
     }
-  }
-  //printf("Total de soluciones %d con pid %d\n",total,getpid());
-  fclose(fp);
-  return 0;
+
+	printf("Soy el proceso h1 %d \n",getpid());
+    n= atoi(argv[1]);
+    //Inicializa el tablero
+    tablero = (int **)malloc(sizeof(int *)*n);
+    for(i=0;i<n;i++)
+     tablero[i]=(int *)malloc(sizeof(int)*n);
+    //Termina inicializacion del tablero
+	
+    for(int hijos=0; hijos<n;hijos++){
+	    tid=fork();
+
+	    if(tid!=0){ //Parte del abanico que funciona
+            //sleep(2);
+            //wait();
+            //printf("Totales %d",total);
+        }
+        
+        else{
+            printf("Soy el h%d: %d y mi papa es: %d \n",hijos+2,getpid(),getppid());
+            //Codigo que debe ejecutar cada hijo
+            for(j=0;j<n;j++){
+                for(k=0;k<n;k++){
+                
+                    //printf("pid %d\n",getppid());
+                    inicializa(tablero,n);
+                    tablero[hijos][k] = 1;
+                    soluciones=backtrackCaballos(tablero,hijos,k,2,n);
+                    *total=*total+soluciones;
+                    //printf("Soluciones en %d %d con pid %d = %d\n",j,k,getpid(),soluciones);
+                    //printf("Parcial de soluciones de todo el tablero = %d con pid %d \n",total,getpid());
+                    char cadena[40];
+                    char cadena2[58];
+                    sprintf(cadena,"\nSoluciones en %d %d con pid %d = %d\n",hijos,k,getpid(),soluciones);
+                    sprintf(cadena2,"\nParcial de soluciones de todo el tablero = %d con pid %d \n",*total,getpid());
+                    fp = fopen ( "fichero.txt", "a" );
+                    fwrite(cadena,sizeof(char),sizeof(cadena),fp);
+                    fwrite(cadena2,sizeof(char),sizeof(cadena2),fp);
+                    fclose(fp);
+                    //j++;
+                    //sleep(2);
+                    //exit(0);
+                            
+
+                }
+                
+                //sleep(2);
+                exit(1);
+                
+                
+            }
+            
+            
+
+
+        }
+	}
+
+    printf("todas las soluciones= %d",*total);
+    munmap(total, sizeof(int));
+    //sleep(1);
+    return 0;
 }
