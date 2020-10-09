19617e5bdf9c96138d37f6b8584e888e5273b7e0
IsraBsal <53355895+IsraBsal@users.noreply.github.com>
2020-10-09 16:50:10 -0500

    Funcionando 100%
    Se hace de forma concurrente, el padre le el archivo que cada hijo hizo con las soluciones que encontró.

--- saltocaballo2.c
+++ saltocaballo2.c
@@ -61,80 +61,84 @@ int backtrackCaballos(int **tablero, int x, int y, int salto, int n){
 
 
 int main(int argc, char **argv){
-    int *total = (int*)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE,
-               MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-
+   int total = 0;
+   //Internas del padre
+   int hijos;
+   int parcial=0;
+   int final=0;
+   //-------------
 	int tid;
 	int n;
-    int i, j, k; //Contadores para irnos moviendo
-    int **tablero;
+   int i, j, k; 
+   int **tablero;
    
-    pid_t pid;
-    FILE *fp; 
+   pid_t pid;
+   FILE *fp; 
     
-    if(argc!=2){
-        printf("Error, ejectutar como ./ejecutable <Tam tablero>, por ejemplo:\n./saltos 7\n");
-    }
+   if(argc!=2){
+      printf("Error, ejectutar como ./ejecutable <Tam tablero>, por ejemplo:\n./saltos 7\n");
+   }
 
-	printf("Soy el proceso h1 %d \n",getpid());
-    n= atoi(argv[1]);
-    //Inicializa el tablero
-    tablero = (int **)malloc(sizeof(int *)*n);
-    for(i=0;i<n;i++)
-     tablero[i]=(int *)malloc(sizeof(int)*n);
-    //Termina inicializacion del tablero
+	//printf("Soy el proceso h1 %d \n",getpid());
+   n= atoi(argv[1]);
+    
+   //Inicializa el tablero
+   tablero = (int **)malloc(sizeof(int *)*n);
+   for(i=0;i<n;i++)
+      tablero[i]=(int *)malloc(sizeof(int)*n);
+   //Termina inicializacion del tablero
 	
-    for(int hijos=0; hijos<n;hijos++){
+
+   //Abanico para crear a los hijos de un solo padre
+   for(int hijos=0; hijos<n;hijos++){
 	    tid=fork();
 
-	    if(tid!=0){ //Parte del abanico que funciona
-            //sleep(2);
-            //wait();
-            //printf("Totales %d",total);
-        }
-        
+	    if(tid!=0  && wait(NULL)!=0){ 
+            //Para esperar a los hijos, es codigo que ejcuta el padre
+         }
         else{
-            printf("Soy el h%d: %d y mi papa es: %d \n",hijos+2,getpid(),getppid());
-            //Codigo que debe ejecutar cada hijo
-            for(j=0;j<n;j++){
-                for(k=0;k<n;k++){
-                
-                    //printf("pid %d\n",getppid());
-                    inicializa(tablero,n);
-                    tablero[hijos][k] = 1;
-                    soluciones=backtrackCaballos(tablero,hijos,k,2,n);
-                    *total=*total+soluciones;
-                    //printf("Soluciones en %d %d con pid %d = %d\n",j,k,getpid(),soluciones);
-                    //printf("Parcial de soluciones de todo el tablero = %d con pid %d \n",total,getpid());
-                    char cadena[40];
-                    char cadena2[58];
-                    sprintf(cadena,"\nSoluciones en %d %d con pid %d = %d\n",hijos,k,getpid(),soluciones);
-                    sprintf(cadena2,"\nParcial de soluciones de todo el tablero = %d con pid %d \n",*total,getpid());
-                    fp = fopen ( "fichero.txt", "a" );
-                    fwrite(cadena,sizeof(char),sizeof(cadena),fp);
-                    fwrite(cadena2,sizeof(char),sizeof(cadena2),fp);
-                    fclose(fp);
-                    //j++;
-                    //sleep(2);
-                    //exit(0);
-                            
-
-                }
-                
-                //sleep(2);
-                exit(1);
-                
-                
-            }
-            
-            
-
-
-        }
+           //Codigo que realiza cada hijo
+           for(k=0;k<n;k++){
+               inicializa(tablero,n);
+               tablero[hijos][k] = 1;
+               soluciones=backtrackCaballos(tablero,hijos,k,2,n);
+               total=total+soluciones;
+            } 
+            char suma[10];
+            char archivo[9];
+            sprintf(archivo,"%d.txt",hijos);//Para nombrar al archivo
+            sprintf(suma,"%d",total);//para escribir el valor que calculo el hijo en su archivo
+            fp = fopen ( archivo, "w" );
+            fputs(suma,fp);//Escribimos
+            fclose(fp);
+            exit(1);
+         }
+            //Termina codigo que realiza cada hijo y se muere
 	}
+   //Termina abanico
 
-    printf("todas las soluciones= %d",*total);
-    munmap(total, sizeof(int));
-    //sleep(1);
+   //Similiar a JOIN De hilos, esperamos a que terminen todos los hijos
+   for(j=0;j<n;j++){
+      if(tid!=0){
+         wait(NULL);
+      }
+   }
+   //Termina Join
+
+   //Leemos cada archivo que el hijo crea
+   for ( int z = 0; z < n; z++){
+      char archivo[10];
+      sprintf(archivo,"%d.txt",z);
+      fp=fopen(archivo,"r");
+      rewind(fp);
+      fscanf(fp,"%d",&parcial);
+      final=final+parcial;
+      //printf("Padre %d, valor que leo del archivo= %d\n",getpid(),final);
+      fclose(fp);
+   }
+   //Terminamos de leer 
+   
+   //El papa imprime la solcuion despues deleer todos los archivos de los hijos
+    printf("todas las soluciones= %d",final);
     return 0;
 }
