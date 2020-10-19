7b006170e457f46131325d1c1544dd2a7b0ed460
IsraBsal <53355895+IsraBsal@users.noreply.github.com>
2020-10-18 21:28:17 -0500

    Nueva version.
    Version para revision.

--- Practica 3 Entregable/src/uam/so/semaforos/Semaforo.java
+++ Practica 3 Entregable/src/uam/so/semaforos/Semaforo.java
@@ -1,38 +1,42 @@
 package uam.so.semaforos;
-
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
 
 public class Semaforo {
+	private ConcurrentLinkedQueue<Thread> cola = new ConcurrentLinkedQueue<Thread>();
+	private int value;
 
-	private final int num_semaforo;
-	private int contador_usados;
+	private Thread hilo=null;
 
 	public Semaforo(int num_semaforo) {
-		this.num_semaforo = num_semaforo;
+		this.value = num_semaforo;
 	}
 
-	public synchronized void acquire() {
-		while(!(recursoDisponible() && posibleWait())) {
-			try {
-				wait();
-			} catch(InterruptedException e) {
-			}
+
+	public void acquire() throws InterruptedException {
+		if(value>0) {
+			value--;	
 		}
+		else {
+			//System.out.println("Entre al acquire en el else");
+			hilo = Thread.currentThread();
+			cola.add(hilo);
+			LockSupport.park();
 
-		contador_usados++;
-	}
-
-	public synchronized void release() {
-		if (contador_usados > 0) {
-			contador_usados--;
-			notifyAll();
 		}
 	}
 
-	private boolean recursoDisponible() {
-		return contador_usados < num_semaforo;
+	public void release() {
+		if(cola.isEmpty()) {
+			value++;
+			//System.out.println("Entre al release en el if");
+		}
+		else {
+			//System.out.println("Entre al release en el ELSE");
+			hilo=cola.remove();
+			LockSupport.unpark(hilo);
+		}
 	}
 
-	private boolean posibleWait() {
-		return true;
-	}
 }
