ed3171ebd448ffa1b343ff5d7acc7f444666de5c
IsraBsal <53355895+IsraBsal@users.noreply.github.com>
2020-11-09 18:56:51 -0600

    Entrega para revision

--- Practica 3 /src/uam/so/semaforos/Semaforo.java
+++ Practica 3 /src/uam/so/semaforos/Semaforo.java
@@ -1,42 +1,46 @@
 package uam.so.semaforos;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.LockSupport;
 
 public class Semaforo {
 	private ConcurrentLinkedQueue<Thread> cola = new ConcurrentLinkedQueue<Thread>();
-	private int value;
+	private final AtomicInteger value;
+	private final int Num_permisos;
 
-	private Thread hilo=null;
+	private Thread propietario=null;
+
 
 	public Semaforo(int num_semaforo) {
-		this.value = num_semaforo;
+		value = new AtomicInteger(num_semaforo);
+		Num_permisos=num_semaforo;
 	}
 
-
-	public void acquire() throws InterruptedException {
-		if(value>0) {
-			value--;	
-		}
-		else {
-			//System.out.println("Entre al acquire en el else");
-			hilo = Thread.currentThread();
-			cola.add(hilo);
+	public void acquire() {
+		boolean interrumpido = false;
+		Thread hilo = Thread.currentThread();
+		cola.add(hilo);
+		//Dormimos a todos los procesos si no soy el del principio de la cola o no hay permisos
+		while (cola.peek() !=hilo || value.getAndDecrement()<Num_permisos) { 
+			value.getAndIncrement(); //Para no llegar a valores negativos y ciclarme
 			LockSupport.park();
-
+			//System.out.println("Thread: "+hilo.getId()+"-> Lock 1");
+			if (Thread.interrupted()) // Verifica si el hilo fue interumpido
+				interrumpido= true;
+			
 		}
+		//Quitamos al hilo que esta al principio de la cola para que trabaje
+		propietario=cola.remove();
+		LockSupport.unpark(propietario);
+		if (interrumpido)          // reassert interrupt status on exit
+			hilo.interrupt(); 
 	}
 
 	public void release() {
-		if(cola.isEmpty()) {
-			value++;
-			//System.out.println("Entre al release en el if");
-		}
-		else {
-			//System.out.println("Entre al release en el ELSE");
-			hilo=cola.remove();
-			LockSupport.unpark(hilo);
-		}
+		LockSupport.unpark(cola.peek());
+		value.getAndIncrement();
 	}
+
+
 
 }
